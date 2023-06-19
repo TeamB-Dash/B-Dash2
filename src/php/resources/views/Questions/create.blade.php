00d86b01446e90191ad35d6544210a6e2281ef40
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-06-19 09:44:36 +0900

    投稿フォームを微修正

--- src/php/resources/views/Questions/create.blade.php
+++ src/php/resources/views/Questions/create.blade.php
@@ -15,7 +15,7 @@
                         <!--Title input-->
                         <div class="mb-6">
                             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">title</label>
-                            <input type="text" name="title" value="{{$question->title}}" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                            <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         </div>
 
                         <!--Tag input-->
@@ -32,7 +32,7 @@
                                 <label
                                 class="inline-block pl-[0.15rem] hover:cursor-pointer"
                                 for=""
-                                >{{$tag->name}}</label
+                                >タグ</label
                                 >
                             </div>
                             @endforeach
