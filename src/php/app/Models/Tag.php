2ffebdfba3e2aa76822f43522bb2e991f531c176
Kazumean <kazumin.angelav@gmail.com>
2023-06-21 16:12:23 +0900

    解消できていなかったコンフリクトを解消。

--- src/php/app/Models/Tag.php
+++ src/php/app/Models/Tag.php
@@ -2,32 +2,25 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use App\Models\MonthlyReport;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Question;
->>>>>>> develop
 
 class Tag extends Model
 {
     use HasFactory;
-
-<<<<<<< HEAD
-    // Tagへの関連を定義
-    public function tags(){
-        return $this->belongsToMany(MonthlyReport::class,'monthly_report_tags');
-=======
+    
     protected $fillable = [
         'name'
     ];
+    // Tagへの関連を定義
+    public function tags(){
+        return $this->belongsToMany(MonthlyReport::class,'monthly_report_tags');
+    }
 
     // Questionへの関連を定義
     public function questions(){
         return $this->belongsToMany(Question::class,'question_tags')->where('is_deleted','=',false)->withTimestamps();
->>>>>>> develop
     }
 }
