e044b61286c61a5aa591c6a84babbe54f463917d
nm <naotom.76@gmail.com>
2023-06-23 05:23:14 +0900

    Routeを追加

--- src/php/routes/web.php
+++ src/php/routes/web.php
@@ -25,7 +25,9 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile/following/destroy{id}', [ProfileController::class, 'followingUserDestroy'])->name('following.destroy');
+    Route::post('/profile/followed/destroy{id}', [ProfileController::class, 'followedUserDestroy'])->name('followed.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
