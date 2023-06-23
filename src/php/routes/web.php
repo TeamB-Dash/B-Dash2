6702b228c8bec44b58c139bae18303b0f2009a44
naotomyogi <124221937+naotomyogi@users.noreply.github.com>
2023-06-23 11:54:07 +0900

    重複の削除

--- src/php/routes/web.php
+++ src/php/routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MonthlyReportController;
 use App\Models\MonthlyReport;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,7 +30,6 @@ Route::resource('/questions', QuestionController::class);
 Route::get('/questions/users/{id}', [QuestionController::class, 'showMyQuestions'])->name('questions.showMyQuestions');
 Route::get('/questions/users/{id}/drafts', [QuestionController::class, 'showMyDraftQuestions'])->name('questions.showMyDraftQuestions');
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -40,17 +38,16 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile/followed/destroy{id}', [ProfileController::class, 'followedUserDestroy'])->name('followed.destroy');
 });
 
-require __DIR__ . '/auth.php';
 Route::resource('/articles', ArticleController::class)
-    ->middleware('auth');
+->middleware('auth');
 Route::resource('/articles', ArticleController::class)
-    ->only(['show']);
+->only(['show']);
 
 Route::get('/articles/users/{id}', [ArticleController::class, 'showArticles'])->name('articles.myblog');
-
-require __DIR__ . '/auth.php';
 
 // 月報関連のルート
 Route::get('/monthly_reports', [MonthlyReportController::class, 'index'])->name('monthlyReport.index');
 Route::get('/monthly_reports/create', [MonthlyReportController::class, 'create'])->name('monthlyReport.create');
 Route::get('/monthly_reports/{monthlyReport}', [MonthlyReportController::class, 'show'])->name('monthlyReport.show');
+
+require __DIR__ . '/auth.php';
