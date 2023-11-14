3506bacf7ccb429b99009f1f6753bd0c27850a49
TadunaKitamura <taduna.kitamura0324@gmail.com>
2023-11-14 14:48:23 +0900

    リプライ編集ボタンの削除

--- src/php/resources/views/Questions/show.blade.php
+++ src/php/resources/views/Questions/show.blade.php
@@ -154,9 +154,9 @@
                                     </div>
                                 </form>
                             </div>
-                            <button type="button" class="edit-reply-button inline-block rounded ml-2 px-4 py-2 text-xs font-medium uppercase leading-normal text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300" data-reply-id="{{ $reply->id }}">
+                            {{-- <button type="button" class="edit-reply-button inline-block rounded ml-2 px-4 py-2 text-xs font-medium uppercase leading-normal text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300" data-reply-id="{{ $reply->id }}">
                                 編集する
-                            </button>
+                            </button> --}}
                             <form action="{{ route('questions.replyDestroy', ['question' => $question->id, 'reply' => $reply->id]) }}" method="POST" class="inline-block delete-reply-form">
                                 @csrf
                                 @method('DELETE')
